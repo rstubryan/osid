407221e91d515ce23aecf2cd159c1c57d65122a0
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-08-31 21:44:44 +0700

    initial commit

---
+++ storage/logs/log-2023-08-31.php
@@ -0,0 +1,131 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+NOTICE - 2023-08-31 21:26:06 --> Berhasil Jalankan migrasi_fitur_premium_2009
+NOTICE - 2023-08-31 21:26:06 --> Berhasil Jalankan migrasi_fitur_premium_2010
+NOTICE - 2023-08-31 21:26:06 --> Berhasil Jalankan migrasi_fitur_premium_2011
+NOTICE - 2023-08-31 21:26:06 --> Berhasil Jalankan migrasi_fitur_premium_2012
+NOTICE - 2023-08-31 21:26:06 --> Berhasil Jalankan migrasi_fitur_premium_2101
+NOTICE - 2023-08-31 21:26:07 --> Berhasil Jalankan migrasi_fitur_premium_2102
+NOTICE - 2023-08-31 21:26:07 --> Berhasil Jalankan migrasi_fitur_premium_2103
+NOTICE - 2023-08-31 21:26:07 --> Berhasil Jalankan migrasi_fitur_premium_2104
+NOTICE - 2023-08-31 21:26:07 --> Berhasil Jalankan migrasi_fitur_premium_2105
+NOTICE - 2023-08-31 21:26:07 --> Berhasil Jalankan migrasi_fitur_premium_2106
+NOTICE - 2023-08-31 21:26:07 --> Berhasil Jalankan migrasi_fitur_premium_2107
+NOTICE - 2023-08-31 21:26:07 --> Berhasil Jalankan migrasi_fitur_premium_2108
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_fitur_premium_2109
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_2203_ke_2204
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_fitur_premium_2110
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_2204_ke_2205
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_fitur_premium_2111
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_2205_ke_2206
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_fitur_premium_2112
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_2206_ke_2207
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_fitur_premium_2201
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_2207_ke_2208
+NOTICE - 2023-08-31 21:26:08 --> Berhasil Jalankan migrasi_foto_aparatur
+NOTICE - 2023-08-31 21:26:09 --> Berhasil Jalankan migrasi_fitur_premium_2202
+NOTICE - 2023-08-31 21:26:09 --> Berhasil Jalankan migrasi_2208_ke_2209
+NOTICE - 2023-08-31 21:26:09 --> Berhasil Jalankan migrasi_fitur_premium_2203
+NOTICE - 2023-08-31 21:26:09 --> Berhasil Jalankan migrasi_2209_ke_2210
+NOTICE - 2023-08-31 21:26:09 --> Berhasil Jalankan migrasi_fitur_premium_2204
+NOTICE - 2023-08-31 21:26:09 --> Berhasil Jalankan migrasi_2210_ke_2211
+NOTICE - 2023-08-31 21:26:10 --> Berhasil Jalankan migrasi_fitur_premium_2205
+NOTICE - 2023-08-31 21:26:10 --> Berhasil Jalankan migrasi_2211_ke_2212
+NOTICE - 2023-08-31 21:26:10 --> Berhasil Jalankan migrasi_fitur_premium_2206
+NOTICE - 2023-08-31 21:26:10 --> Berhasil Jalankan migrasi_2212_ke_2301
+NOTICE - 2023-08-31 21:26:11 --> Berhasil Jalankan migrasi_fitur_premium_2207
+NOTICE - 2023-08-31 21:26:11 --> Berhasil Jalankan migrasi_2301_ke_2302
+NOTICE - 2023-08-31 21:26:11 --> Berhasil Jalankan migrasi_fitur_premium_2208
+NOTICE - 2023-08-31 21:26:11 --> Berhasil Jalankan migrasi_2302_ke_2303
+NOTICE - 2023-08-31 21:26:12 --> Berhasil Jalankan migrasi_fitur_premium_2209
+NOTICE - 2023-08-31 21:26:12 --> Berhasil Jalankan migrasi_2303_ke_2304
+NOTICE - 2023-08-31 21:26:13 --> Berhasil Jalankan migrasi_fitur_premium_2210
+NOTICE - 2023-08-31 21:26:13 --> Berhasil Jalankan migrasi_2304_ke_2305
+NOTICE - 2023-08-31 21:26:13 --> Berhasil Jalankan migrasi_fitur_premium_2211
+NOTICE - 2023-08-31 21:26:13 --> Berhasil Jalankan migrasi_2305_ke_2306
+NOTICE - 2023-08-31 21:26:13 --> Berhasil Jalankan migrasi_dtks
+NOTICE - 2023-08-31 21:26:13 --> Berhasil Jalankan migrasi_fitur_premium_2212
+NOTICE - 2023-08-31 21:26:13 --> Berhasil Jalankan migrasi_2306_ke_2307
+NOTICE - 2023-08-31 21:26:14 --> Berhasil Jalankan migrasi_fitur_premium_2301
+NOTICE - 2023-08-31 21:26:14 --> Berhasil Jalankan migrasi_2307_ke_2308
+NOTICE - 2023-08-31 21:26:14 --> Berhasil Jalankan migrasi_fitur_premium_2302
+NOTICE - 2023-08-31 21:26:14 --> Berhasil Jalankan migrasi_2308_ke_2309
+NOTICE - 2023-08-31 21:26:14 --> Berhasil Jalankan migrasi_layanan
+NOTICE - 2023-08-31 21:26:16 --> Versi database sudah terbaru
+NOTICE - 2023-08-31 16:27:12 --> Jalankan tabel_analisis
+NOTICE - 2023-08-31 16:27:12 --> Jalankan tabel_mandiri
+NOTICE - 2023-08-31 16:27:12 --> Jalankan tabel_peta
+NOTICE - 2023-08-31 16:27:13 --> Jalankan tabel_web
+NOTICE - 2023-08-31 16:27:13 --> Jalankan tabel_pertanahan
+NOTICE - 2023-08-31 16:27:13 --> Jalankan tabel_desa
+NOTICE - 2023-08-31 16:27:13 --> Jalankan tabel_covid19
+NOTICE - 2023-08-31 16:27:13 --> Jalankan tabel_penduduk
+NOTICE - 2023-08-31 16:27:13 --> Jalankan tabel_dokumen
+NOTICE - 2023-08-31 16:27:13 --> Jalankan tabel_hubung_warga
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_inventaris
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_buku_admin
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_keuangan
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_surat
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_sinkronisasi
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_dashboard
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_pembangunan
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_lapak
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_bantuan
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_sistem
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_pamong
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_keluarga
+NOTICE - 2023-08-31 16:27:14 --> Jalankan tabel_pengguna
+NOTICE - 2023-08-31 16:27:16 --> Jalankan tabel_ada_foreign_key
+NOTICE - 2023-08-31 16:27:16 --> Jalankan buat_view
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2009
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2010
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2011
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2012
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2101
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2102
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2103
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2104
+NOTICE - 2023-08-31 16:27:21 --> Berhasil Jalankan migrasi_fitur_premium_2105
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_fitur_premium_2106
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_fitur_premium_2107
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_fitur_premium_2108
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_fitur_premium_2109
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_2203_ke_2204
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_fitur_premium_2110
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_2204_ke_2205
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_fitur_premium_2111
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_2205_ke_2206
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_fitur_premium_2112
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_2206_ke_2207
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_fitur_premium_2201
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_2207_ke_2208
+NOTICE - 2023-08-31 16:27:22 --> Berhasil Jalankan migrasi_foto_aparatur
+NOTICE - 2023-08-31 16:27:23 --> Berhasil Jalankan migrasi_fitur_premium_2202
+NOTICE - 2023-08-31 16:27:23 --> Berhasil Jalankan migrasi_2208_ke_2209
+NOTICE - 2023-08-31 16:27:23 --> Berhasil Jalankan migrasi_fitur_premium_2203
+NOTICE - 2023-08-31 16:27:23 --> Berhasil Jalankan migrasi_2209_ke_2210
+NOTICE - 2023-08-31 16:27:23 --> Berhasil Jalankan migrasi_fitur_premium_2204
+NOTICE - 2023-08-31 16:27:23 --> Berhasil Jalankan migrasi_2210_ke_2211
+NOTICE - 2023-08-31 16:27:24 --> Berhasil Jalankan migrasi_fitur_premium_2205
+NOTICE - 2023-08-31 16:27:24 --> Berhasil Jalankan migrasi_2211_ke_2212
+NOTICE - 2023-08-31 16:27:24 --> Berhasil Jalankan migrasi_fitur_premium_2206
+NOTICE - 2023-08-31 16:27:24 --> Berhasil Jalankan migrasi_2212_ke_2301
+NOTICE - 2023-08-31 16:27:25 --> Berhasil Jalankan migrasi_fitur_premium_2207
+NOTICE - 2023-08-31 16:27:25 --> Berhasil Jalankan migrasi_2301_ke_2302
+NOTICE - 2023-08-31 16:27:25 --> Berhasil Jalankan migrasi_fitur_premium_2208
+NOTICE - 2023-08-31 16:27:25 --> Berhasil Jalankan migrasi_2302_ke_2303
+NOTICE - 2023-08-31 16:27:26 --> Berhasil Jalankan migrasi_fitur_premium_2209
+NOTICE - 2023-08-31 16:27:26 --> Berhasil Jalankan migrasi_2303_ke_2304
+NOTICE - 2023-08-31 16:27:26 --> Berhasil Jalankan migrasi_fitur_premium_2210
+NOTICE - 2023-08-31 16:27:26 --> Berhasil Jalankan migrasi_2304_ke_2305
+NOTICE - 2023-08-31 16:27:27 --> Berhasil Jalankan migrasi_fitur_premium_2211
+NOTICE - 2023-08-31 16:27:27 --> Berhasil Jalankan migrasi_2305_ke_2306
+NOTICE - 2023-08-31 16:27:27 --> Berhasil Jalankan migrasi_dtks
+NOTICE - 2023-08-31 16:27:27 --> Berhasil Jalankan migrasi_fitur_premium_2212
+NOTICE - 2023-08-31 16:27:27 --> Berhasil Jalankan migrasi_2306_ke_2307
+NOTICE - 2023-08-31 16:27:28 --> Berhasil Jalankan migrasi_fitur_premium_2301
+NOTICE - 2023-08-31 16:27:28 --> Berhasil Jalankan migrasi_2307_ke_2308
+NOTICE - 2023-08-31 16:27:28 --> Berhasil Jalankan migrasi_fitur_premium_2302
+NOTICE - 2023-08-31 16:27:28 --> Berhasil Jalankan migrasi_2308_ke_2309
+NOTICE - 2023-08-31 16:27:28 --> Berhasil Jalankan migrasi_layanan
+NOTICE - 2023-08-31 16:27:28 --> Versi database sudah terbaru
